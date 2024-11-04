0c357c62e847774dbf9b7b981fa60a6966c44d20
emje-dania <emje7@student.eadania.dk>
2024-11-04 10:57:31 +0100

    Setup from class diagram
    What was in the starter diagram has been added to GameObject

--- PirateGame/PirateGame/GameObject.cs
+++ PirateGame/PirateGame/GameObject.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,44 @@ using System.Threading.Tasks;
 
 namespace PirateGame
 {
-    internal class GameObject
+    abstract class GameObject
     {
+        // Field
+        protected Texture2D sprite;
+        protected Texture2D[] sprites;
+        protected Vector2 velocity;
+        protected Vector2 position;
+        protected float speed;
+        protected float fps; // the animation speed
+        private float timeElapsed; // time passed since frame changed
+        private int currentIndex; // Index of current frame
+
+        // Properties
+        public Rectangle collisionBox { get; }
+
+        // Methods
+        public abstract void LoadContent(ContentManager content);
+        public abstract void Update(GameTime gameTime);
+       
+        public void Draw (SpriteBatch spriteBatch)
+        {
+
+        }
+        protected void Move (GameObject other)
+        {
+
+        }
+        public void OnCollision(GameObject other)
+        {
+
+        }
+        public void CheckCollision (GameObject other)
+        {
+
+        }
+        public void TakeDamage()
+        {
+
+        }
     }
 }
