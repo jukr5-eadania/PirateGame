cd28c93db4840dd745544bce71f37b4930100ed5
emje-dania <emje7@student.eadania.dk>
2024-11-04 14:14:03 +0100

    imported skeleton animation & setup

--- PirateGame/PirateGame/Enemy.cs
+++ PirateGame/PirateGame/Enemy.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,49 @@ using System.Threading.Tasks;
 
 namespace PirateGame
 {
-    internal class Enemy
+    /// <summary>
+    /// "Enemy" is a sup-class from GameObject.
+    /// </summary>
+    abstract class Enemy : GameObject
     {
+        // Field
+        
+        private int health;
+
+
+        // Properties
+
+        // Methods
+        public override void LoadContent(ContentManager content)
+        {
+            // make the idle animation
+            sprites = new Texture2D[7];
+            for(int i = 0; i < sprites.Length; i++)
+            {
+                sprites[i] = content.Load<Texture2D>($"Skeleton_White_{i}");
+            }
+            //set a default sprite
+            sprite = sprites[0];
+        }
+        public void OnCollision (GameObject other)
+        {
+
+        }
+        public void TakeDamage()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            throw new NotImplementedException();
+        }
+
 
     }
 }
